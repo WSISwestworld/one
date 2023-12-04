e406d07764ecc325f4b1acbac37e73dfaa0f5d0d
Amaya De Silva <111060924+AmayaDS@users.noreply.github.com>
2023-12-04 06:13:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,3 @@ window.onscroll = function () { sysPanel() };
 var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
 
-function sysPanel() {
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky")
-    } else {
-        navbar.classList.remove("sticky");
-    }
-}
